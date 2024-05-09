d9f01c05d1a8ef9bf71858d150f5c4da4235fdf3
David G Tech <76923507+davidgrcias@users.noreply.github.com>
2024-05-09 20:03:47 +0700

    done by david

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,79 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import "./Header.css";
 
 function Header() {
   return (
     <>
       {/* Navigation */}
-      <nav
-        className="navbar navbar-expand-lg navbar-light fixed-top"
-        id="mainNav"
-      >
-        <div className="container px-4 px-lg-5">
-          <Link className="navbar-brand" to="/">
+      <Helmet>
+        <link rel="stylesheet" href="templatetravela/lightbox.min.css" />
+        <link rel="stylesheet" href="templatetravela/owl.carousel.min.css" />
+        <link rel="stylesheet" href="templatetravela/bootstrap.min.css" />
+      </Helmet>
+
+      <div className="container-fluid position-relative p-0">
+        <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+          <NavLink className="navbar-brand" to="/">
             <img
               src="/assets/img/wi.png"
               alt="Start Bootstrap"
               style={{
-                height: "6em",
-                marginTop: "-14px",
-                paddingBottom: "0px",
+                height: "10em",
               }}
             />
-          </Link>
+          </NavLink>
           <button
-            className="navbar-toggler navbar-toggler-right"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            data-bs-target="#navbarCollapse"
           >
-            <span>Menu</span>
-            <i className="fas fa-bars"></i>
+            <span className="fa fa-bars" />
           </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  <i class="fa-solid fa-house"></i> Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/Things-to-do">
-                  <i class="fas fa-tasks"></i> Things to do
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/About">
-                  <i class="fa-solid fa-people-group"></i> About Us
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/Game">
-                  <i class="fa-solid fa-gamepad"></i> Game
-                </Link>
-              </li>
-            </ul>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="navbar-nav ms-auto py-0">
+              <NavLink
+                to="/"
+                className="nav-item nav-link"
+                exact
+                activeClassName="active"
+                id="link"
+              >
+                <i className="fa-solid fa-house"></i> Home
+              </NavLink>
+              <NavLink
+                to="/Things-to-do"
+                className="nav-item nav-link"
+                activeClassName="active"
+                id="link"
+              >
+                <i className="fas fa-tasks"></i> Things to do
+              </NavLink>
+              <NavLink
+                to="/About"
+                className="nav-item nav-link"
+                activeClassName="active"
+                id="link"
+              >
+                <i className="fa-solid fa-people-group"></i> About Us
+              </NavLink>
+              <NavLink
+                to="/Game"
+                className="nav-item nav-link"
+                activeClassName="active"
+                id="link"
+              >
+                <i className="fa-solid fa-gamepad"></i> Game
+              </NavLink>
+            </div>
+            {/*<a href className="btn btn-primary rounded-pill py-2 px-4 ms-lg-4">
+              Contact
+            </a>*/}
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 }
