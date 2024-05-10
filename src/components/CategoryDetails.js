5776aaf73233b885a1e92dfc0aaf1ab14b234294
David G Tech <76923507+davidgrcias@users.noreply.github.com>
2024-05-11 04:33:36 +0700

    done

--- src/components/CategoryDetails.js
+++ src/components/CategoryDetails.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { useParams } from "react-router-dom";
 
-function CategoryDetails({ category }) {
+function CategoryDetails() {
+  const { category } = useParams(); // Use useParams to get the category
   return (
     <div>
       <Helmet>
         <link rel="stylesheet" href="templatetravela/halo.css" />
       </Helmet>
-      <h1> {category}</h1>
+      <h1>{category}</h1> {/* Display the category */}
     </div>
   );
 }
